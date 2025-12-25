19aab3891e551c963b114a33c92796a47558392b
Nethmihimasha <nethmi123himasha@gmail.com>
2025-12-25 21:07:49 +0530

    added career to the nav bar

--- app/layout.js
+++ app/layout.js
@@ -44,8 +44,10 @@ function Header() {
   <Link href="/about_us" style={styles.navLink} className="nav-link">About</Link>
   <Link href="/services" style={styles.navLink} className="nav-link">Services</Link>
   <Link href="/portfolio" style={styles.navLink} className="nav-link">Portfolio</Link>
+  <Link href="/career" style={styles.navLink} className="nav-link">Career</Link>
   <Link href="/review" style={styles.navLink} className="nav-link">Review Us</Link>
   <Link href="/contact" style={styles.navLink} className="nav-link">Contact</Link>
+  
 </nav>
         <NavLinkActive />
         <button style={styles.ctaButton}>Get Started</button>
